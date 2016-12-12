150ba70eafecd279a49f103a5a45735820e7ef77
Olga Sizova <olisizova@gmail.com>
2016-12-12 11:36:28 -0500

    updated model

--- models/bikeit_model.js
+++ models/bikeit_model.js
@@ -13,6 +13,8 @@ const db = pg(config);
 
 module.exports = {
 
+// person model
+
   getPerson(req, res, next){
 
     let filter = '';
@@ -33,7 +35,7 @@ module.exports = {
     let filter = "where id = " + req.params.id;
     let notes = req.params.notes;
 
-    db.query("UPDATE person SET notes = '" + notes + "'" + filter)
+    db.query("UPDATE bikeit SET notes = '" + notes + "'" + filter)
     .then((arrRecords) => {
       res.returnRecords = arrRecords;
       next();
@@ -64,6 +66,140 @@ module.exports = {
                  + "', '" + req.params.lname
                  + "', '" + req.params.fname
                  + "', '" + req.params.imgurl
+                 + "')"
+
+    db.query("INSERT INTO bikeit" + fields + "VALUES" + values)
+      .then((arrRecords) => {
+        res.returnRecords = arrRecords;
+        next();
+      })
+      .catch(error => next(error));
+      return false;
+
+  },
+
+// trip model
+
+
+  getTrip(req, res, next){
+
+    let filter = '';
+    if(req.params.id){
+      filter = 'where id =' + req.params.id;
+    }
+
+    db.query('SELECT * FROM bikeit' + filter)
+      .then((arrRecords) => {
+        res.returnRecords = arrRecords;
+        next();
+      })
+      .catch(error => next(error));
+      return false;
+  },
+
+  updateTrip(req, res, next) {
+    let filter = "where id = " + req.params.id;
+    let notes = req.params.notes;
+
+    db.query("UPDATE bikeit SET notes = '" + notes + "'" + filter)
+    .then((arrRecords) => {
+      res.returnRecords = arrRecords;
+      next();
+    })
+    .catch(error => next));
+    return false;
+  },
+
+  deleteTrip(req, res, next) {
+
+    let filter = " where id = " + req.params.id;
+    let notes = req.params.notes;
+
+    db.query("DELETE FROM bikeit" + filter)
+    .then((arrRecords) => {
+      res.returnRecords = arrRecords;
+      next();
+    })
+    .catch(error => next(error));
+    return false;
+  },
+
+  insertTrip(req, res, next) {
+
+    let fields = " (country, city, start_addr, end_addr, meet_date, meet_time)";
+    let values = " ('" + req.params.country
+                 + "', '" + req.params.city
+                 + "', '" + req.params.start_addr
+                 + "', '" + req.params.end_addr
+                 + "', '" + req.params.meet_date
+                 + "', '" + req.params.meet_time
+                 + "')"
+
+    db.query("INSERT INTO bikeit" + fields + "VALUES" + values)
+      .then((arrRecords) => {
+        res.returnRecords = arrRecords;
+        next();
+      })
+      .catch(error => next(error));
+      return false;
+
+  }
+},
+
+
+
+
+
+// team model
+
+getTeam(req, res, next){
+
+    let filter = '';
+    if(req.params.id){
+      filter = 'where id =' + req.params.id;
+    }
+
+    db.query('SELECT * FROM bikeit' + filter)
+      .then((arrRecords) => {
+        res.returnRecords = arrRecords;
+        next();
+      })
+      .catch(error => next(error));
+      return false;
+  },
+
+  updateTeam(req, res, next) {
+    let filter = "where id = " + req.params.id;
+    let notes = req.params.notes;
+
+    db.query("UPDATE bikeit SET notes = '" + notes + "'" + filter)
+    .then((arrRecords) => {
+      res.returnRecords = arrRecords;
+      next();
+    })
+    .catch(error => next));
+    return false;
+  },
+
+  deleteTeam(req, res, next) {
+
+    let filter = " where id = " + req.params.id;
+    let notes = req.params.notes;
+
+    db.query("DELETE FROM bikeit" + filter)
+    .then((arrRecords) => {
+      res.returnRecords = arrRecords;
+      next();
+    })
+    .catch(error => next(error));
+    return false;
+  },
+
+  insertTeam(req, res, next) {
+
+    let fields = " (trip_id, person_id)";
+    let values = " ('" + req.params.trip_id
+                 + "', '" + req.params.person_id
                  + "')"
 
     db.query("INSERT INTO bikeit" + fields + "VALUES" + values)
